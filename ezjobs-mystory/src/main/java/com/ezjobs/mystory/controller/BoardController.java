945e965cb139a82cb1649991311e84d38f1da5ff
blacktokki <39031723+blacktokki@users.noreply.github.com>
2019-10-30 23:04:25 +0900

    롤백

--- ezjobs-mystory/src/main/java/com/ezjobs/mystory/controller/BoardController.java
+++ ezjobs-mystory/src/main/java/com/ezjobs/mystory/controller/BoardController.java
@@ -2,6 +2,7 @@ package com.ezjobs.mystory.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import com.ezjobs.mystory.entity.Board;
 import com.ezjobs.mystory.service.BoardService;
 
 import java.util.Map;
@@ -9,7 +10,7 @@ import java.util.Map;
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+//import org.springframework.ui.Model;
 
 
 
@@ -27,28 +28,27 @@ public class BoardController {
 	private BoardService boardService;
 	
 	@GetMapping("/community")//커뮤니티 게시판 목록 /board/community
-	public String community(@RequestParam Map<Object,Object> map,Model model){
-		model.addAttribute("map",map);
-		boardService.community(model);
+	public String community(@RequestParam String page,Map<Object, Object> map){
+		map.put("page",page);
+		boardService.community(map);
 		return "board/list";
 	}
 	
 	@GetMapping("/content/{id}")//글내용 보기 /board/content
-	public String content(@PathVariable String id,Model model){
-		model.addAttribute("id",id);
-		boardService.content(model);
+	public String content(@PathVariable String id,Map<Object, Object> map){
+		map.put("id",id);
+		boardService.content(map);
 		return "board/content";
 	}
 	
 	@GetMapping("/write")//글작성 화면 /board/write
-	public String writeView(){
+	public String writeView(Map<Object, Object> map){
 		return "board/write";
 	}
 	
 	@PostMapping("/write")//글작성 요청 /board/write
-	public String Write(@RequestParam Map<Object,Object> map,Model model){
-		model.addAttribute("map",map);
-		boardService.write(model);
+	public String Write(@RequestParam Map<Object,Object> map){
+		boardService.write(map);
 		return "redirect:community";
 	}
 	
--- ezjobs-mystory/src/main/java/com/ezjobs/mystory/service/BoardService.java
+++ ezjobs-mystory/src/main/java/com/ezjobs/mystory/service/BoardService.java
@@ -4,11 +4,11 @@ import java.util.Map;
 import java.util.Optional;
 
 import javax.inject.Inject;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.Model;
 
 import com.ezjobs.mystory.entity.Board;
 import com.ezjobs.mystory.repository.BoardRepository;
@@ -23,15 +23,13 @@ public class BoardService {
 	@Inject
 	ObjectMapper mapper;
 	
-	public void community(Model model){
-		Map<String,Object> modelMap=model.asMap();
-		Map<?,?> map=(Map<?, ?>)modelMap.get("map");
-		String page=Optional.ofNullable((String)map.get("page")).orElse("1");//String으로 담음
-		int pageNum=Integer.parseInt(page)-1;//값이없을경우 0
+	public void community(Map<Object,Object> map){
+		Optional<String> page=Optional.ofNullable((String)map.get("page"));//String으로 담음
+		int pageNum=Integer.parseInt(page.orElse("1"))-1;//값이없을경우 0
 		PageRequest pr=PageRequest.of(pageNum, 3,Sort.by(Sort.Direction.DESC,"editDate"));
 		Page<Board> boards=boardRepository.findAll(pr);//pr을 기준으로 검색
-		model.addAttribute("boards",boards);
-		model.addAttribute("pageNavNumber",boards.getNumber()/5);//페이징바의 번호
+		map.put("boards",boards);
+		map.put("pageNavNumber",boards.getNumber()/5);//페이징바의 번호
 		//System.out.println(boards.getNumberOfElements());
 		//System.out.println(boards.getSize());
 		//System.out.println(boards.getNumber());
@@ -40,17 +38,14 @@ public class BoardService {
 		
 	}
 
-	public void content(Model model) {
-		Map<String,Object> modelMap=model.asMap();
-		int id=Integer.parseInt(modelMap.get("id").toString());
+	public void content(Map<Object, Object> map) {
+		int id=Integer.parseInt(map.get("id").toString());
 		Board board=boardRepository.findById(id).get();//id로 board 찾기
-		model.addAttribute("board",board);
+		map.put("board",board);
 	}
 
-	public void write(Model model){
-		Map<String,Object> modelMap=model.asMap();
-		Map<?,?> map=(Map<?, ?>)modelMap.get("map");
-		Board board=mapper.convertValue(map, Board.class);//board로 변환
+	public void write(Map<Object, Object> map) {
+		Board board=mapper.convertValue(map, Board.class);//map을 board로 변환
 		boardRepository.save(board);
 	}
 }

0689eb894db7264da751632caebf4ca646b351c5
blacktokki <39031723+blacktokki@users.noreply.github.com>
2019-10-30 23:15:57 +0900

    Develop prepare (#4)
    * init
    
    * 각주,webjar,페이징 추가
    
    * url 파라미터 버그수정
    
    * 롤백
    
    * url 파라미터 버그수정

--- ezjobs-mystory/src/main/java/com/ezjobs/mystory/controller/BoardController.java
+++ ezjobs-mystory/src/main/java/com/ezjobs/mystory/controller/BoardController.java
@@ -2,7 +2,6 @@ package com.ezjobs.mystory.controller;
 
 import org.springframework.web.bind.annotation.*;
 
-import com.ezjobs.mystory.entity.Board;
 import com.ezjobs.mystory.service.BoardService;
 
 import java.util.Map;
@@ -10,7 +9,7 @@ import java.util.Map;
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
+import org.springframework.ui.Model;
 
 
 
@@ -28,27 +27,28 @@ public class BoardController {
 	private BoardService boardService;
 	
 	@GetMapping("/community")//커뮤니티 게시판 목록 /board/community
-	public String community(@RequestParam String page,Map<Object, Object> map){
-		map.put("page",page);
-		boardService.community(map);
+	public String community(@RequestParam Map<Object,Object> map,Model model){
+		model.addAttribute("map",map);
+		boardService.community(model);
 		return "board/list";
 	}
 	
 	@GetMapping("/content/{id}")//글내용 보기 /board/content
-	public String content(@PathVariable String id,Map<Object, Object> map){
-		map.put("id",id);
-		boardService.content(map);
+	public String content(@PathVariable String id,Model model){
+		model.addAttribute("id",id);
+		boardService.content(model);
 		return "board/content";
 	}
 	
 	@GetMapping("/write")//글작성 화면 /board/write
-	public String writeView(Map<Object, Object> map){
+	public String writeView(){
 		return "board/write";
 	}
 	
 	@PostMapping("/write")//글작성 요청 /board/write
-	public String Write(@RequestParam Map<Object,Object> map){
-		boardService.write(map);
+	public String Write(@RequestParam Map<Object,Object> map,Model model){
+		model.addAttribute("map",map);
+		boardService.write(model);
 		return "redirect:community";
 	}
 	

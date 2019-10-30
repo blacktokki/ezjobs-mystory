e8ce8c0fc36ba4c76aba139e28a382cc8b7c1621
blacktokki <39031723+blacktokki@users.noreply.github.com>
2019-10-30 22:44:45 +0900

    url 파라미터 버그수정

--- ezjobs-mystory/src/main/java/com/ezjobs/mystory/service/BoardService.java
+++ ezjobs-mystory/src/main/java/com/ezjobs/mystory/service/BoardService.java
@@ -4,11 +4,11 @@ import java.util.Map;
 import java.util.Optional;
 
 import javax.inject.Inject;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
 import com.ezjobs.mystory.entity.Board;
 import com.ezjobs.mystory.repository.BoardRepository;
@@ -23,13 +23,15 @@ public class BoardService {
 	@Inject
 	ObjectMapper mapper;
 	
-	public void community(Map<Object,Object> map){
-		Optional<String> page=Optional.ofNullable((String)map.get("page"));//String으로 담음
-		int pageNum=Integer.parseInt(page.orElse("1"))-1;//값이없을경우 0
+	public void community(Model model){
+		Map<String,Object> modelMap=model.asMap();
+		Map<?,?> map=(Map<?, ?>)modelMap.get("map");
+		String page=Optional.ofNullable((String)map.get("page")).orElse("1");//String으로 담음
+		int pageNum=Integer.parseInt(page)-1;//값이없을경우 0
 		PageRequest pr=PageRequest.of(pageNum, 3,Sort.by(Sort.Direction.DESC,"editDate"));
 		Page<Board> boards=boardRepository.findAll(pr);//pr을 기준으로 검색
-		map.put("boards",boards);
-		map.put("pageNavNumber",boards.getNumber()/5);//페이징바의 번호
+		model.addAttribute("boards",boards);
+		model.addAttribute("pageNavNumber",boards.getNumber()/5);//페이징바의 번호
 		//System.out.println(boards.getNumberOfElements());
 		//System.out.println(boards.getSize());
 		//System.out.println(boards.getNumber());
@@ -38,14 +40,17 @@ public class BoardService {
 		
 	}
 
-	public void content(Map<Object, Object> map) {
-		int id=Integer.parseInt(map.get("id").toString());
+	public void content(Model model) {
+		Map<String,Object> modelMap=model.asMap();
+		int id=Integer.parseInt(modelMap.get("id").toString());
 		Board board=boardRepository.findById(id).get();//id로 board 찾기
-		map.put("board",board);
+		model.addAttribute("board",board);
 	}
 
-	public void write(Map<Object, Object> map) {
-		Board board=mapper.convertValue(map, Board.class);//map을 board로 변환
+	public void write(Model model){
+		Map<String,Object> modelMap=model.asMap();
+		Map<?,?> map=(Map<?, ?>)modelMap.get("map");
+		Board board=mapper.convertValue(map, Board.class);//board로 변환
 		boardRepository.save(board);
 	}
 }

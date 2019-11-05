1d4e9d611f09acbbb0f12bff50a541baaf69d3a7
alsgh <alsgh@DESKTOP-FIEP42A>
2019-11-05 22:56:55 +0900

    http://localhost:8080/user/userLogin?page=1 페이지 작동 확인

--- ezjobs-mystory/src/main/java/com/ezjobs/mystory/service/UserService.java
+++ ezjobs-mystory/src/main/java/com/ezjobs/mystory/service/UserService.java
@@ -8,12 +8,16 @@ import javax.inject.Inject;
 import org.springframework.stereotype.Service;
 
 import com.ezjobs.mystory.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class UserService {
 	
 	@Inject
 	UserRepository userRepository;
+	
+	@Inject
+	ObjectMapper mapper;
 
 	public void userLogin(Map<Object, Object> map) {
 		// TODO Auto-generated method stub

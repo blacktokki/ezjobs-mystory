8bf47c115c7cda7d358ba8a74ef561ef5d7b3cb2
blacktokki <39031723+blacktokki@users.noreply.github.com>
2019-11-10 00:02:05 +0900

    Search기능과의 충돌해결시도

--- ezjobs-mystory/src/main/java/com/ezjobs/mystory/repository/ResumeRepository.java
+++ ezjobs-mystory/src/main/java/com/ezjobs/mystory/repository/ResumeRepository.java
@@ -1,20 +1,15 @@
 package com.ezjobs.mystory.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.ezjobs.mystory.entity.Resume;
 
 @Repository
 public interface ResumeRepository extends JpaRepository<Resume, Integer>{
-	@Transactional
-	@Modifying	// update , delete Query
-	@Query(value="update Resume r set r.question = :#{#resume.question}, r.answer = :#{#resume.answer} "
-			+ " WHERE r.id = :#{#resume.id}")
-	void update(@Param("resume") Resume resume);
-
+	Page<Resume> findByTypeContainingOrDeptContainingOrCompanyContainingOrQuestionContainingOrAnswerContainingOrUserIdContaining(Pageable pageable, String type, String dept, String company, String question, String answer, String userId);
+	Page<Resume> findByQuestionContaining(Pageable pageable, String question);
+	Page<Resume> findByAnswerContaining(Pageable pageable, String answer);
 }

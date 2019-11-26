7240ccb97c298091aa3d8d61abc77d4223a15139
blacktokki <39031723+blacktokki@users.noreply.github.com>
2019-11-26 18:04:03 +0900

    board유형의 게시글만 불러오게 변경

--- ezjobs-mystory/src/main/java/com/ezjobs/mystory/service/BoardService.java
+++ ezjobs-mystory/src/main/java/com/ezjobs/mystory/service/BoardService.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import java.util.Optional;
 
 import javax.inject.Inject;
+
+import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -30,7 +32,9 @@ public class BoardService {
 		String page=Optional.ofNullable((String)map.get("page")).orElse("1");//String으로 담음
 		int pageNum=Integer.parseInt(page)-1;//값이없을경우 0
 		PageRequest pr=PageRequest.of(pageNum, 3,Sort.by(Sort.Direction.DESC,"editDate"));
-		Page<Board> boards=boardRepository.findAll(pr);//pr을 기준으로 검색
+		Board board=new Board();
+		board.setBoardType("board");
+		Page<Board> boards=boardRepository.findAll(Example.of(board), pr);//pr을 기준으로 검색
 		model.addAttribute("boards",boards);
 		model.addAttribute("pageNavNumber",boards.getNumber()/5);//페이징바의 번호
 		//System.out.println(boards.getNumberOfElements());
@@ -55,6 +59,7 @@ public class BoardService {
 		Board board=mapper.convertValue(map, Board.class);//board로 변환
 		board.setUserId(userId);
 		board.setEditDate(new Date());
+		board.setBoardType("board");
 		boardRepository.save(board);
 	}
 	

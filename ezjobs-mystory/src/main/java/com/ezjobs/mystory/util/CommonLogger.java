d3f4acee205ef5991bd6699212820793d18c01d3
blacktokki <39031723+blacktokki@users.noreply.github.com>
2020-02-22 19:49:31 +0900

    jsp 성능 측정용 로깅 구현

--- ezjobs-mystory/src/main/java/com/ezjobs/mystory/util/CommonLogger.java
+++ ezjobs-mystory/src/main/java/com/ezjobs/mystory/util/CommonLogger.java
@@ -1,5 +1,7 @@
 package com.ezjobs.mystory.util;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -7,14 +9,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
-@Aspect
 @Component
+@Aspect
 public class CommonLogger {
-    Logger logger =  LoggerFactory.getLogger(CommonLogger.class);
+    static final Logger logger =  LoggerFactory.getLogger(CommonLogger.class);
+    
+    public static Logger getLogger() {
+    	return logger;
+    }
     
     @Around("execution(* com.ezjobs.mystory..*.*(..))")
-    public Object logging(ProceedingJoinPoint pjp) throws Throwable {
+    public Object loggingDefault(ProceedingJoinPoint pjp) throws Throwable {
     	 StopWatch stopWatch = new StopWatch();
         logger.info("start - " + pjp.getSignature().getDeclaringTypeName() + " / " + pjp.getSignature().getName());
         stopWatch.start();
@@ -22,6 +30,16 @@ public class CommonLogger {
         stopWatch.stop();
         logger.info("finished - " + pjp.getSignature().getDeclaringTypeName() + " / " + pjp.getSignature().getName()
         	+" / " + stopWatch.getTotalTimeMillis() + "(ms)");
+        return result;
+    }
+    
+    @Around("execution(* com.ezjobs.mystory.controller..*.*(..))")
+    public Object loggingController(ProceedingJoinPoint pjp) throws Throwable {
+        Object result = pjp.proceed();
+        long start = System.currentTimeMillis();
+	    logger.info("Elapsed before Jsp: " + (System.currentTimeMillis() - start));
+	    HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getRequest();
+	    request.setAttribute("startTime", start);
         return result;
     }
 }
